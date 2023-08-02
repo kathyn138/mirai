605771c52d26f09e2990ad63eefdca80e0ab06cc
Kathy Nguyen <kathyn262@gmail.com>
2023-08-01 20:35:19 -0700

    fin setup food data gather

--- selenium/fetchSpecialDishes.py
+++ selenium/fetchSpecialDishes.py
@@ -1,4 +1,4 @@
-## https://www.geeksforgeeks.org/download-google-image-using-python-and-selenium
+# https://www.geeksforgeeks.org/download-google-image-using-python-and-selenium
 # https://stackoverflow.com/questions/64799002/how-to-loop-through-a-list-of-urls-using-selenium-and-python
 # https://www.geeksforgeeks.org/how-to-scrape-multiple-pages-using-selenium-in-python/
 
@@ -17,10 +17,13 @@ url = 'https://genshin-impact.fandom.com/wiki/Food#List_of_Special_Dishes'
 driver.get(url)
 table = driver.find_element("xpath", '//*[@id="mw-content-text"]/div/table[4]/tbody')
 rows = table.find_elements(By.TAG_NAME, 'tr')
-name = rows[0].find_elements(By.TAG_NAME, 'a')[1].get_attribute('innerHTML')
 # prints Item "My Way"
-quality = rows[0].find_element(By.TAG_NAME, 'img').get_attribute('alt')
-print(quality)
+name = rows[0].find_elements(By.TAG_NAME, 'a')[1].get_attribute('innerHTML')
+quality = rows[0].find_elements(By.TAG_NAME, 'img')[1].get_attribute('alt').split()[0]
+dish_type = rows[0].find_elements(By.TAG_NAME, 'td')[3].get_attribute('innerText')
+effect = rows[0].find_elements(By.TAG_NAME, 'td')[4].get_attribute('innerText')
+character = rows[0].find_elements(By.TAG_NAME, 'td')[5].get_attribute('innerText')
+print(character)
 # src = img.get_attribute('src')
 # urllib.request.urlretrieve(src, food[i])
 
