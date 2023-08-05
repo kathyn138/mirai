79df84f6c4a9783496e9d95fbacb720bafc6f349
Kathy Nguyen <kathyn262@gmail.com>
2023-08-04 20:08:20 -0700

    setup formatted data

--- selenium/fetchSpecialDishes.py
+++ selenium/fetchSpecialDishes.py
@@ -18,12 +18,35 @@ driver.get(url)
 table = driver.find_element("xpath", '//*[@id="mw-content-text"]/div/table[4]/tbody')
 rows = table.find_elements(By.TAG_NAME, 'tr')
 # prints Item "My Way"
-name = rows[0].find_elements(By.TAG_NAME, 'a')[1].get_attribute('innerHTML')
+official_name = rows[0].find_elements(By.TAG_NAME, 'a')[1].get_attribute('innerHTML')
+tag_name = official_name.lower().replace(' ', '-')
 quality = rows[0].find_elements(By.TAG_NAME, 'img')[1].get_attribute('alt').split()[0]
+# remove plural for dish_type
 dish_type = rows[0].find_elements(By.TAG_NAME, 'td')[3].get_attribute('innerText')
 effect = rows[0].find_elements(By.TAG_NAME, 'td')[4].get_attribute('innerText')
 character = rows[0].find_elements(By.TAG_NAME, 'td')[5].get_attribute('innerText')
-print(character)
+formattedData = (f' {tag_name}: {{\n '
+    f' "name": {official_name},\n '
+    f' "rarity": {quality},\n '
+    f' "type": "{dish_type}",\n '
+    f' "effect": "{effect}",\n '
+    f' "hasRecipe": true,\n '
+    f' "description": "???",\n '
+    f' "character": "{character}",\n '
+    f' "proficiency": "??",\n '
+    f' "baseDish": "??",\n '
+    f' "recipe": [\n '
+    f'   {{\n '
+        f' "item": "??",\n '
+        f' "quantity": 3\n '
+      f' }},\n '
+      f' {{\n '
+        f' "item": "",\n '
+        f' "quantity": 2\n '
+      f' }}\n '
+    f' ]\n '
+  f'}},')
+print(formattedData)
 # src = img.get_attribute('src')
 # urllib.request.urlretrieve(src, food[i])
 
